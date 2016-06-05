533563b58c983349bd917678816a575198cd32f5
Kalle Lindström <karl@dewire.com>
2016-06-05 11:54:47 +0200

    Initial Commit

---
+++ DeHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeHub
+//
+//  Created by Kalle Lindström on 05/06/16.
+//  Copyright © 2016 Dewire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
